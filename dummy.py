c0c69237e0b5edb0a71ca83751e08fe38edfe215
NAVNATHBHOSKAR <navnathbhoskar1998@gmail.com>
2023-02-18 16:36:16 +0530

    git sikhle sale

--- dummy.py
+++ dummy.py
@@ -2,4 +2,6 @@ print("1) git status >>> ")
 print("2) Do Some Changes in File >>>")
 print("3) git status >>> ") # red modified:   filename.py
 print("4) git add .")
-print("3) git status >>> ")  # green modified:   filename.py
+print("5) git status >>> ")  # green modified:   filename.py
+print('6) git commit -m "git sikhle sale"') # commit kar or msg likh
+print("7) git push") #Git Push karle
