936158f52a6c25086daa3c44a97ae17cdcdc7006
NAVNATHBHOSKAR <navnathbhoskar1998@gmail.com>
2023-02-18 16:32:13 +0530

    git sikhle sale

--- dummy.py
+++ dummy.py
@@ -1,3 +1,5 @@
-print("hello world")
-print("Nava")
-print("Aaj ke liye bas")
+print("1) git status >>> ")
+print("2) Do Some Changes in File >>>")
+print("3) git status >>> ") # red modified:   filename.py
+print("4) git add .")
+print("3) git status >>> ")  # green modified:   filename.py
