eb434ae7839d7145947c4f1fed10950be790f247
NAVNATHBHOSKAR <navnathbhoskar1998@gmail.com>
2023-02-18 16:46:13 +0530

    Aaj ke liye bas

--- gitbasic.py
+++ gitbasic.py
@@ -1,7 +1,7 @@
-print("1) git status >>> ")
+print("1) git status >>> Before doing any changes")
 print("2) Do Some Changes in File >>>")
-print("3) git status >>> ") # red modified:   filename.py
+print("3) git status >>> After doing any changes") # red modified:   filename.py
 print("4) git add .")
-print("5) git status >>> ")  # green modified:   filename.py
+print("5) git status >>> After adding to git")  # green modified:   filename.py
 print('6) git commit -m "git sikhle sale"') # commit kar or msg likh
 print("7) git push") #Git Push karle
